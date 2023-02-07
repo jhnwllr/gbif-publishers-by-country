b63bbcd4404c471b3c50edb820a5aad780fd4b13
John Waller <ftw712@ku.dk>
2023-02-07 15:30:28 +0100

    adding actions

--- shell/export_parquet.sh
+++
@@ -1,8 +0,0 @@
-FILE_NAME=$1
-rm -r  /mnt/auto/misc/download.gbif.org/custom_download/jwaller/$FILE_NAME".parquet.zip"
-hadoop fs -get /user/jwaller/$FILE_NAME".parquet"  /mnt/auto/misc/download.gbif.org/custom_download/jwaller/$FILE_NAME".parquet"
-hdfs dfs -rm -r $FILE_NAME".parquet"
-cd /mnt/auto/misc/download.gbif.org/custom_download/jwaller/
-zip -r $FILE_NAME".parquet.zip" $FILE_NAME".parquet"
-rm -r  /mnt/auto/misc/download.gbif.org/custom_download/jwaller/$FILE_NAME".parquet"
-exit
